3d424d204d425b2154fc293cfed53378bc8398e1
indugunturu <indugunturustockholm@gmail.com>
2023-07-25 17:48:52 +0530

    changed username

--- src/components/User.js
+++ src/components/User.js
@@ -14,7 +14,7 @@ const User = ({ name }) => {
       <h1>Count2 = {count2}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: Dehradun</h3>
-      <h4>Contact: @akshaymarch7</h4>
+      <h4>Contact: 1234567</h4>
     </div>
   );
 };
